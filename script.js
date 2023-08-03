971c769ef46b97521dc671bb8ecd9ea7d89ee8e6
fareeza05 <fareezarahman123@gmail.com>
2023-08-02 21:30:38 -0400

    js event listeners added

--- script.js
+++ script.js
@@ -4,5 +4,30 @@ const email = document.getElementById('email');
 const num = document.getElementById('phone');
 const pass = document.getElementById('password');
 const confirmation = document.getElementById('confirm-pass');
-const errorDisplay = document.querySelector('.error');
+const errorDisplay = document.querySelectorAll('.error');
+const inputField = document.querySelectorAll('input');
 const submitBtn = document.getElementById('submit');
+
+// for first name, & last name --> already validated
+let count = 2;
+
+// to display blank error string upon validation of a field
+function Valid(currentErrorDisplay, messageString, booleanTest) {
+    console.log('Valid function called:', currentErrorDisplay, messageString, booleanTest);
+    currentErrorDisplay.textContent = messageString; // sets message to string passed through func
+    booleanTest != 0 ? ++count : count = count // 0 = false, meaning input not valid so count of valid inputs does not increase.
+} 
+
+// listening to every input field upon clicking
+for(let i=0; i<inputField.length ; i++){
+    let currentField = inputField[i]
+    let currentErrorField = errorDisplay[i]
+
+    currentField.addEventListener('keyup', (e)=>{
+        console.log('Keyup event triggered:', e.target.value);
+        let msg = currentErrorField
+        e.target.value != "" ? Valid(msg, '', 0) : Valid(msg, '* This field is required', 0)
+    })
+}
+
+//
